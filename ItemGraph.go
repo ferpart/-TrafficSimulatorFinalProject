ebeb8a13b551828cd76a104253f8326df13d03b5
Fernando Partida <ferpart@hotmail.com>
2019-05-11 23:31:50 -0500

    ItemGraph code reordered

--- ItemGraph.go
+++ ItemGraph.go
@@ -16,6 +16,32 @@ type Node struct {
 	visited       bool
 }
 
+//ItemGraph : Node storage (graph).
+type ItemGraph struct {
+	nodes []*Node
+	edges map[*Node][]*Node
+	lock  sync.RWMutex
+}
+
+//AddNode : used to add a node to the graph.
+func (g *ItemGraph) AddNode(n *Node) {
+	g.lock.Lock()
+	g.nodes = append(g.nodes, n)
+	g.lock.Unlock()
+}
+
+//AddEdge : addes an edge to the graph
+func (g *ItemGraph) AddEdge(n1, n2 *Node) {
+	g.lock.Lock()
+	if g.edges == nil {
+		g.edges = make(map[*Node][]*Node)
+	}
+	g.edges[n1] = append(g.edges[n1], n2)
+	// g.edges[*n2] = append(g.edges[*n2], n1)
+
+	g.lock.Unlock()
+}
+
 func (n *Node) getCar() *Car {
 	return n.mCar
 }
@@ -63,30 +89,4 @@ func (n *Node) setHasCar(hasCar bool) {
 
 func (n *Node) getisFinal() bool {
 	return n.isFinal
-}
-
-//ItemGraph : Node storage (graph).
-type ItemGraph struct {
-	nodes []*Node
-	edges map[*Node][]*Node
-	lock  sync.RWMutex
-}
-
-//AddNode : used to add a node to the graph.
-func (g *ItemGraph) AddNode(n *Node) {
-	g.lock.Lock()
-	g.nodes = append(g.nodes, n)
-	g.lock.Unlock()
-}
-
-//AddEdge : addes an edge to the graph
-func (g *ItemGraph) AddEdge(n1, n2 *Node) {
-	g.lock.Lock()
-	if g.edges == nil {
-		g.edges = make(map[*Node][]*Node)
-	}
-	g.edges[n1] = append(g.edges[n1], n2)
-	// g.edges[*n2] = append(g.edges[*n2], n1)
-
-	g.lock.Unlock()
 }
