9e1eede1578135d5105639fe1137e2b902ba616c
Cesar Augusto Garcia Perez <cesarau04@gmail.com>
2019-05-14 16:39:30 -0500

    working with colors

--- Semaphore.go
+++ Semaphore.go
@@ -15,14 +15,16 @@ type Semaphore struct {
 	mutex    *sync.Mutex
 }
 
-func (s *Semaphore) acquireTurn(name string) {
+func (s *Semaphore) acquireTurn(id int, name string) {
 	semaphoreNode := g.nodes[getIndex(name)]
 	for {
 		s.mutex.Lock()
+		semaphoresInMap[id] = true
 		fmt.Printf("\t\t==== SEMAPHORE %s ====\n", name)
 		semaphoreNode.setSemaphorState(true)
 		time.Sleep(2 * time.Second)
 		s.mutex.Unlock()
+		semaphoresInMap[id] = false
 		semaphoreNode.setSemaphorState(false)
 		time.Sleep(6 * time.Second)
 	}
